19e58c0055cce6600c1046750fd230ddda4ec1ab
ChiragKumar-09 <71137952+ChiragKumar-09@users.noreply.github.com>
2020-09-14 19:25:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 Sound = "";
+leftWristX = 0;
+rightWristX = 0;                  
+leftWristY = 0;
+rightWristY = 0;      
 
 function preload() {
     Sound = loadSound("music.mp3");
@@ -11,6 +15,9 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide()
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
 
 function draw() {
@@ -19,4 +26,23 @@ function draw() {
 
 function play() {
     Sound.play();
+    Sound.setVolume(1);
+    Sound.rate(1);
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized");
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+       console.log(results);
+
+       leftWristX = results[0].pose.leftWrist.x;
+       leftWristY = results[0].pose.leftWrist.y;
+       rightWristX = results[0].pose.rightWrist.x;
+       rightWristY = results[0].pose.rightWrist.y;
+
+       console.log("leftWristX = "+leftWristX+"leftWristY = "+leftWristY);
+       console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
+    }
 }
